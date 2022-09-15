4c62986565b4c82c3999d1761a8d6ba1d4f91cea
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-15 11:15:01 +0530

    Update settings.py

--- adifect/settings.py
+++ adifect/settings.py
@@ -109,23 +109,23 @@ WSGI_APPLICATION = 'adifect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'adifect',
-#         'USER': 'postgres',
-#         'PASSWORD': 'studio45#',
-#         'HOST': '192.168.1.245',
-#         'port':'5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'adifect',
+        'USER': 'postgres',
+        'PASSWORD': 'studio45#',
+        'HOST': '192.168.1.245',
+        'port':'5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
