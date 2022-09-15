fa37d7dd665160360437a3da6aeb72a2ba3153d6
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-15 20:23:00 +0530

    Update settings.py

--- adifect/settings.py
+++ adifect/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-^r8dr13s8wlx5kkvg6)g3dha7=4mtangi6=@xo&ac5v)1x8cb4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
