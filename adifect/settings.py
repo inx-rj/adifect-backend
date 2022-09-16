fc40320e439d78cdeff6b8cfba3ee6cc501ea747
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-16 11:09:23 +0530

    Update settings.py

--- adifect/settings.py
+++ adifect/settings.py
@@ -142,7 +142,8 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
