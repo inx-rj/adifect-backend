a777892f250f492600d7780f1a0856a36929a614
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-16 15:12:53 +0530

    Update settings.py

--- adifect/settings.py
+++ adifect/settings.py
@@ -234,8 +234,8 @@ TWILIO_AUTH_TOKEN2 = ''
 #----------------- end ------------------------------#
 
 
-# DB_NAME = os.environ.get('DB_NAME')
-# USER_NAME = os.environ.get('DB_USER')
-# DB_PASSWORD = os.environ.get('DB_PASSWORD')
-# DB_HOST = os.environ.get('DB_HOST')
-# DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+USER_NAME = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
