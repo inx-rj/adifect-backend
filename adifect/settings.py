25f9f8da62677466fa923e6bac068f475657ae2d
vijaystudio45 <vijaya.logiclab@gmail.com>
2022-09-19 19:28:57 +0530

    setting.py

--- adifect/settings.py
+++ adifect/settings.py
@@ -160,8 +160,8 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 #     'https://dev.adifect.com'
 # ]
 
-# CORS_ALLOWED_ORIGINS = ['*']
-CORS_ALLOWED_ORIGINS = ['https://'+os.environ.get('FRONTEND_SITE_URL')]
+CORS_ALLOWED_ORIGINS = ['*']
+# CORS_ALLOWED_ORIGINS = ['https://'+os.environ.get('FRONTEND_SITE_URL')]
 
 SEND_GRID_API_key = os.environ.get('SEND_GRID_API_KEY')
 SEND_GRID_FROM_EMAIL = os.environ.get('SEND_GRID_FROM_EMAIL')
