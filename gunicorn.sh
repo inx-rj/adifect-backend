6d196d83971dcce8a6e4321879194a73a0d32d3e
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-23 23:57:25 +0530

    Update gunicorn.sh
    migration 8

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 python manage.py makemigrations --noinput
-python manage.py makemigrations agency
-python manage.py makemigrations creator
 python manage.py migrate
 gunicorn adifect.wsgi:application --bind 0.0.0.0:8000  --timeout 600 
