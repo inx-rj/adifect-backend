4fb6a838041559dfa2ae68a453c675e4334cb201
vijaystudio45 <vijaya.logiclab@gmail.com>
2022-09-23 20:14:44 +0530

    migrations commit-2

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-python manage.py makemigrations authentication --noinput
-python manage.py makemigrations administrator --noinput
-python manage.py makemigrations agency --noinput
-python manage.py makemigrations creator --noinput
+python manage.py makemigrations --noinput
 python manage.py migrate
 gunicorn adifect.wsgi:application --bind 0.0.0.0:8000  --timeout 600 
