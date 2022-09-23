3045399264c5c413bd8aaac145f23ebe05d6060e
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-23 23:46:59 +0530

    Update gunicorn.sh
    migration 7

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 python manage.py makemigrations --noinput
-python manage.py makemigrations administrator
 python manage.py makemigrations agency
 python manage.py makemigrations creator
 python manage.py migrate
