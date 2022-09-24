9919ac9c888bd4e572296cde1bbafc9c1c933ed3
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-24 10:18:50 +0530

    Update models.py
    Migrations check

--- agency/models.py
+++ agency/models.py
@@ -172,6 +172,7 @@ class DamMedia(BaseModel):
 
 class NewModel(BaseModel):
     name = models.CharField(max_length=50, default=None)
+    is_active4 = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     is_active2 = models.BooleanField(default=True)
 
