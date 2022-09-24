59919b1af9a525b2b52d2b7cfe457faca6e18e9f
root <root@ip-172-31-8-237.ec2.internal>
2022-09-24 15:15:01 +0000

    update entrypoint

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-python manage.py makemigrations --noinput
-python manage.py migrate
-gunicorn adifect.wsgi:application --bind 0.0.0.0:8000  --timeout 600 
+python manage.py makemigrations authentication
+python manage.py makemigrations administrator
+python manage.py makemigrations agency
+python manage.py makemigrations creator
+sleep 5
+
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and run db migraiton
+while ! python manage.py migrate  2>&1; do
+   echo "Migration is in progress status"
+   sleep 5
+done
+
+#python manage.py migrate
+
+gunicorn adifect.wsgi:application --bind 0.0.0.0:8000  --timeout 600
