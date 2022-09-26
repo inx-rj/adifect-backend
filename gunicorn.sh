5f5d2da5dc31b55ea64cdb2b4869e5ec7a5fcc8e
Vijay <30430062+vijaystudio45@users.noreply.github.com>
2022-09-26 13:08:18 +0530

    Update gunicorn.sh
    Migrations 2

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-python manage.py makemigrations --noinput
-sleep 5
+
+while ! python manage.py flush --no-input 2>&1; do
+  echo "Flusing django manage command"
+  sleep 3
+done
 
 echo "Migrate the Database at startup of project"
 
@@ -9,6 +12,8 @@ while ! python manage.py migrate  2>&1; do
    echo "Migration is in progress status"
    sleep 5
 done
+
+exec "$@"
 
 #python manage.py migrate
 
