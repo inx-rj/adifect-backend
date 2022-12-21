19e19c60f4b4cf05f977da7ad8dd69c2b2ab8bb1
vijaystudio45 <vijaya.logiclab@gmail.com>
2022-12-21 18:52:42 +0530

    redis

--- adifect/settings.py
+++ adifect/settings.py
@@ -126,7 +126,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(f"redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0")],
+            "hosts": [(f"redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}")],
         },
     },
 }
