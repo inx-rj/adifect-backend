1d6c2c0a9e57b1e0fd23123123bfb065430c3ee4
darshan.inexture <darshanbhimani@inexture.com>
2023-03-17 15:25:58 +0530

    feat: Added the migrations file and also loaded models in admin.py file of publication module

--- publication/admin.py
+++ publication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from publication.models import Publication, Article, Tag, ArticleTag
+
 # Register your models here.
+admin.site.register(Publication)
+admin.site.register(Article)
+admin.site.register(Tag)
+admin.site.register(ArticleTag)
