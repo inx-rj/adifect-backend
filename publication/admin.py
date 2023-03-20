882120e10eae14ba41de0bab135255634ead17ab
kush.inexture <kush@inexture.com>
2023-03-20 19:50:56 +0530

    refactor: updated ArticleTag relation in django admin.

--- publication/admin.py
+++ publication/admin.py
@@ -4,6 +4,18 @@ from publication.models import Publication, Article, Tag, ArticleTag
 
 # Register your models here.
 admin.site.register(Publication)
-admin.site.register(Article)
 admin.site.register(Tag)
 admin.site.register(ArticleTag)
+
+
+class ArticleTagAdmin(admin.TabularInline):
+    model = ArticleTag
+    extra = 1
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'lede', 'word_count', 'publication', 'publication_date', 'status', 'body', 'p_url', 'is_active')
+    inlines = (ArticleTagAdmin, )
+
+
