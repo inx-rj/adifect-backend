36d0a91d9aaa98d7303363f0a27846defee62a87
kush.inexture <kush@inexture.com>
2023-03-31 11:19:09 +0530

    refactor: code reverted.

--- community/urls.py
+++ community/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from community.views import StoriesList, CommunityTagsListCreate, CommunityList, TagList, StatusList
+from community.views import StoriesList, CommunityTagsListCreate, CommunityList
 
 urlpatterns = [
     path('stories/', StoriesList.as_view(), name='get_stories'),
     path('tags/', CommunityTagsListCreate.as_view(), name='get_create_community_tags'),
-    path('list-community/', CommunityList.as_view(), name='list_community'),
-    path('list-status/', StatusList.as_view(), name='list_status'),
-    path('list-tag/', TagList.as_view(), name='list_tag')
+    path('list-community-status-tag-data/', CommunityList.as_view(), name='list_community_status_tag'),
+    # path('list-status/', StatusList.as_view(), name='list_status'),
+    # path('list-tag/', TagList.as_view(), name='list_tag')
 ]
