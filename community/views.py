2f3d481770ac8c65475c1e0f8292cbead66907fd
kush.inexture <kush@inexture.com>
2023-06-13 17:10:27 +0530

    refactor: Added checks in update community setting for adding audience.

--- community/views.py
+++ community/views.py
@@ -32,7 +32,7 @@ from community.constants import TAG_CREATED, STORIES_RETRIEVE_SUCCESSFULLY, COMM
     TAG_TO_STORY_ADDED_SUCCESSFULLY, CREATIVE_CODE_DATA_IMPORTED_SUCCESSFULLY
 from community.filters import StoriesFilter
 from community.models import Story, Community, Tag, CommunitySetting, Channel, CommunityChannel, Program, CopyCode, \
-    CreativeCode, StoryTag
+    CreativeCode, StoryTag, Audience
 from community.permissions import IsAuthorizedForListCreate
 from community.serializers import StorySerializer, CommunityTagsSerializer, \
     TagCreateSerializer, CommunitySettingsSerializer, ChannelListCreateSerializer, \
@@ -206,12 +206,18 @@ class CommunitySettingsView(generics.ListCreateAPIView, generics.RetrieveUpdateD
         instance = self.get_object()
         story_data_entry.delay(instance.community.community_id, instance.community.community_id,
                                instance_community_delete=True)
+        Audience.objects.filter(community_id=instance.community.id).update(is_trashed=True)
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
         community_id = instance.community.community_id
+        old_opn_obj = CommunityChannel.objects.filter(community_setting=instance,
+                                                      channel__name__iexact='opnsesame').first()
+        old_opn_url = old_opn_obj.url
+        old_opn_api_key = old_opn_obj.api_key
+        old_community_id = instance.community.id
 
         with transaction.atomic():
             CommunityChannel.objects.filter(community_setting=instance).delete()
@@ -220,7 +226,22 @@ class CommunitySettingsView(generics.ListCreateAPIView, generics.RetrieveUpdateD
                                                               "id": kwargs.get('id')})
             serializer.is_valid(raise_exception=True)
             community_setting_obj = serializer.save()
+            new_opn_obj = CommunityChannel.objects.filter(community_setting=community_setting_obj,
+                                                          channel__name__iexact='opnsesame').first()
             story_data_entry.delay(community_setting_obj.community.community_id, community_id)
+
+            if old_community_id != community_setting_obj.community.id or old_opn_url \
+                    != new_opn_obj.url or old_opn_api_key != new_opn_obj.api_key:
+                Audience.objects.filter(community_id=old_community_id).update(is_trashed=True)
+
+                if new_opn_obj and validate_client_id_opnsesame(client_id=new_opn_obj.url,
+                                                                api_key=new_opn_obj.api_key):
+                    # Call Background task for fetching audiences
+                    logger.info("VALID CLIENT_ID")
+                    logger.info("Calling background task to add audiences.")
+                    add_community_audiences.delay(new_opn_obj.url, new_opn_obj.api_key,
+                                                  community_setting_obj.community_id)
+
         return Response({'data': '', 'message': COMMUNITY_SETTINGS_UPDATE_SUCCESS}, status=status.HTTP_200_OK)
 
 
