ee2f992995389883ebaf112db88b2135faa54ac2
darshan.inexture <darshanbhimani@inexture.com>
2023-07-03 13:41:42 +0530

    feat: Added title, p_url, published_at and updated_at filters in the stories API

--- community/filters.py
+++ community/filters.py
@@ -3,9 +3,12 @@ from django_filters import CharFilter
 
 
 class StoriesFilter(django_filters.FilterSet):
-
     from_date = CharFilter(field_name='publication_date', method='filter_from_date')
     to_date = CharFilter(field_name='publication_date', method='filter_to_date')
+    title = CharFilter(field_name='title', method='filter_title')
+    p_url = CharFilter(field_name='p_url', method='filter_p_url')
+    published_at = CharFilter(field_name='story_metadata.published_at', method='filter_published_at')
+    updated_at = CharFilter(field_name='story_metadata.updated_at', method='filter_updated_at')
     community = CharFilter(field_name='community.name', method='filter_community')
     tag = CharFilter(field_name='tag.title', method='filter_tag')
     status = CharFilter(field_name='status', method='filter_status')
@@ -31,8 +34,25 @@ class StoriesFilter(django_filters.FilterSet):
         self.queryset = self.queryset.filter(tag__title__icontains=value)
         return self.queryset
 
+    def filter_title(self, queryset, status, value):
+        self.queryset = self.queryset.filter(title__icontains=value)
+        return self.queryset
+
+    def filter_p_url(self, queryset, status, value):
+        self.queryset = self.queryset.filter(p_url__icontains=value)
+        return self.queryset
+
+    def filter_published_at(self, queryset, status, value):
+        self.queryset = self.queryset.filter(story_metadata__published_at__icontains=value)
+        return self.queryset
+
+    def filter_updated_at(self, queryset, status, value):
+        self.queryset = self.queryset.filter(story_metadata__updated_at__icontains=value)
+        return self.queryset
+
     def filter_ordering(self, queryset, ordering, value):
-        value_list = ['title', 'p_url', 'community__name', 'status', 'tags__title']
+        value_list = ['title', 'p_url', 'community__name', 'status', 'tags__title', 'story_metadata__published_at',
+                      'story_metadata__updated_at']
         if value in value_list:
             self.queryset = self.queryset.order_by(value)
         return self.queryset
