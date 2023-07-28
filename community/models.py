0592af015638270e55c94956bbce25567f353d34
kush.inexture <kush@inexture.com>
2023-07-28 10:30:36 +0530

    refactor: api_key and url in community settings reverted.

--- community/models.py
+++ community/models.py
@@ -124,6 +124,8 @@ class CommunitySetting(BaseModel):
 class CommunityChannel(BaseModel):
     community_setting = models.ForeignKey(CommunitySetting, related_name='community_channel_community', on_delete=models.SET_NULL, null=True, blank=True)
     channel = models.ForeignKey(Channel, related_name='community_channel_channel', on_delete=models.SET_NULL, null=True, blank=True)
+    url = models.CharField(max_length=200)
+    api_key = models.CharField(max_length=200)
     meta_data = models.JSONField(null=True, blank=True)
 
     class Meta:
